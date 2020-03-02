b500552bf3a1f7940e733c7a3466ee704716c1bd
teha007 <teha007@naver.com>
2020-03-03 00:31:25 +0900

    add

--- js/toDo.js
+++ js/toDo.js
@@ -38,7 +38,7 @@ function addToDo(text) {
     toDo.className = "toDo";
     toDo.id = toDos.length + 1;
     const deleteBtn = document.createElement("span");
-    deleteBtn.innerHTML = "❌";
+    deleteBtn.innerHTML = "X";
     deleteBtn.className = "toDo__button";
     deleteBtn.addEventListener("click", handleDelete);
     const label = document.createElement("label");
