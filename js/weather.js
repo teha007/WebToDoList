95bb3cc4cfcb2c917cbc0b6d29a0bc1112c2541f
teha007 <teha007@naver.com>
2020-03-04 13:21:47 +0900

    add

--- js/weather.js
+++ js/weather.js
@@ -6,50 +6,50 @@ const WEATHER_API = "https://api.openweathermap.org/data/2.5/weather?";
 const weather = document.querySelector(".js-weather .weather__text");
 
 function getWeather(coords) {
-  fetch(
-    `${WEATHER_API}lat=${coords.lat}&lon=${
+    fetch(
+            `${WEATHER_API}lat=${coords.lat}&lon=${
     coords.lng
     }&appid=${API_KEY}&units=metric`
-  )
-    .then(response => response.json())
-    .then(json => {
-      const name = json.name;
-      const temperature = json.main.temp;
-      weather.innerHTML = `${Math.floor(temperature)}°C - ${name}`;
-    });
+        )
+        .then(response => response.json())
+        .then(json => {
+            const name = json.name;
+            const temperature = json.main.temp;
+            weather.innerHTML = `${Math.floor(temperature)}°C - ${name}`;
+        });
 }
 
 function handleGeoSuccess(position) {
-  const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  const coords = {
-    lat,
-    lng
-  };
-  localStorage.setItem("coords", JSON.stringify(coords));
-  getWeather(coords);
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    const coords = {
+        lat,
+        lng
+    };
+    localStorage.setItem("coords", JSON.stringify(coords));
+    getWeather(coords);
 }
 
 function handleGeoFailure() {
-  console.log("no location");
+    console.log("no location");
 }
 
 function loadWeather() {
-  const currentCoords = localStorage.getItem("coords");
-  if (currentCoords !== null) {
-    const parsedCoords = JSON.parse(currentCoords);
-    getWeather(parsedCoords);
-    return;
-  } else {
-    navigator.geolocation.getCurrentPosition(
-      handleGeoSuccess,
-      handleGeoFailure
-    );
-  }
+    const currentCoords = localStorage.getItem("coords");
+    if (currentCoords !== null) {
+        const parsedCoords = JSON.parse(currentCoords);
+        getWeather(parsedCoords);
+        return;
+    } else {
+        navigator.geolocation.getCurrentPosition(
+            handleGeoSuccess,
+            handleGeoFailure
+        );
+    }
 }
 
 function init() {
-  loadWeather();
+    loadWeather();
 }
 
 init();
